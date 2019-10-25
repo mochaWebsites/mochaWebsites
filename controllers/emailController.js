c6800f2b2f3364d72aeb75ddd2c44a4107c9effe
mochaWebsites <ianlawsondevelopment@gmail.com>
2019-10-24 21:59:45 -0400

    styles and layout

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,6 +1,20 @@
 const Email = require('../models/Email.js');
+const { validationResult } = require('express-validator');
 
 exports.postContact = (req, res, err) => {
+  const errors = validationResult(req);
+
+  const messages = errors.array().map(errObj => errObj.msg);
+  console.log(!errors.isEmpty(), messages);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('home', {
+      layout: 'default',
+      template: 'home',
+      errorMessages: messages,
+    });
+  }
+
   const name = req.body.name;
   const fromEmail = req.body.email || 'anonymous';
   const desc = req.body.desc;
--- routes/email.js
+++ routes/email.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { check } = require('express-validator');
 
 const rootDir = require('../util/rootPath');
-const EmailService = require(rootDir + '/controllers/emailController');
+const emailController = require(rootDir + '/controllers/emailController');
 
 const router = express.Router();
 
-router.post('/contact', EmailService.postContact);
+router.post('/contact',
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid Email'),
+  emailController.postContact
+);
 
 module.exports = router;
